a7b91252441db6627e2024ceea9350ac64860d8b
Lokendrasinghparmar <lok42412@gmail.com>
2019-07-02 01:01:07 +0530

    update

--- js/login.js
+++ js/login.js
@@ -1,128 +1,95 @@
-var questions = [
-  {question:"What's your first name?"},
-  {question:"What's your last name?"},
-  {question:"What's your email?", pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/},
-  {question:"Create your password", type: "password"}
-]
 
-/**********
+TweenMax.from(".wrapper .cardShadow .circle1", 1.5, {
+   scale:0.5,
+   transformOrigin:'top left',
+    opacity: 0,
+    ease: Expo.easeInOut
+ });
 
-  !!!!!
-  New Version: https://codepen.io/arcs/pen/rYXrNQ
-  !!!!!
-  
-  Credits for the design go to XavierCoulombeM
-  https://dribbble.com/shots/2510592-Simple-register-form
-  
-  This Pen uses no libraries except fonts and should 
-  work on all modern browsers
-  
-  The answers are stored in the `questions` array
-  with the key `value`. 
+ TweenMax.from(".wrapper .cardShadow .circle2", 1.5, {
+   scale:0.5,
+   transformOrigin:'bottom right',
+    opacity: 0,
+    ease: Expo.easeInOut
+ });
 
- **********/
+ TweenMax.staggerFrom(".block1 form > *",1, {
+   y:-50,
+   scale:0.5,
+    opacity: 0,
+    ease: Expo.easeInOut
+ },0.2);
 
-;(function(){
 
-  var tTime = 100  // transition transform time from #register in ms
-  var wTime = 200  // transition width time from #register in ms
-  var eTime = 1000 // transition width time from inputLabel in ms
 
-  // init
-  // --------------
-  var position = 0
-
-  putQuestion()
-
-  progressButton.addEventListener('click', validate)
-  inputField.addEventListener('keyup', function(e){
-    transform(0, 0) // ie hack to redraw
-    if(e.keyCode == 13) validate()
-  })
-
-  // functions
-  // --------------
-
-  // load the next question
-  function putQuestion() {
-    inputLabel.innerHTML = questions[position].question
-    inputField.value = ''
-    inputField.type = questions[position].type || 'text'  
-    inputField.focus()
-    showCurrent()
-  }
-  
-  // when all the questions have been answered
-  function done() {
-    
-    // remove the box if there is no next question
-    register.className = 'close'
-    
-    // add the h1 at the end with the welcome text
-    var h1 = document.createElement('h1')
-    h1.appendChild(document.createTextNode('Welcome ' + questions[0].value + '!'))
-    setTimeout(function() {
-      register.parentElement.appendChild(h1)     
-      setTimeout(function() {h1.style.opacity = 1}, 50)
-    }, eTime)
-    
-  }
-
-  // when submitting the current question
-  function validate() {
-
-    // set the value of the field into the array
-    questions[position].value = inputField.value
-
-    // check if the pattern matches
-    if (!inputField.value.match(questions[position].pattern || /.+/)) wrong()
-    else ok(function() {
+function rotateit(){
+   
+      TweenMax.to(".wrapper .cardShadow .block1", 1, {
+         rotation:360,
+         transformOrigin:'bottom right',
+         opacity: 0,
+         ease: Expo.easeInOut
+      });
+      TweenMax.to(".wrapper .cardShadow .block2", 1, {
+         rotation:360,
+         opacity: 1,
+         ease: Expo.easeInOut
+      });
+      TweenMax.staggerFrom(".block2 form > *",1, {
+         y:-50,
+         delay:0.5,
+         scale:0.5,
+         opacity: 0,
+         ease: Expo.easeInOut
+       },0.2);
+       TweenMax.from(".wrapper .cardShadow .circle1", 1.5, {
+         scale:0.5,
+         transformOrigin:'top left',
+          opacity: 0,
+          ease: Expo.easeInOut
+       },'-=1');
       
-      // set the progress of the background
-      progress.style.width = ++position * 100 / questions.length + 'vw'
+       TweenMax.from(".wrapper .cardShadow .circle2", 1.5, {
+         scale:0.5,
+         transformOrigin:'bottom right',
+          opacity: 0,
+          ease: Expo.easeInOut
+       },'-=1');
+  
 
-      // if there is a new question, hide current and load next
-      if (questions[position]) hideCurrent(putQuestion)
-      else hideCurrent(done)
-             
-    })
+}
 
-  }
+function rotateit1(){
+   
+      TweenMax.to(".wrapper .cardShadow .block2", 1, {
+         rotation:180,
+         opacity:0,
+         ease: Expo.easeInOut
+      });
+      TweenMax.to(".wrapper .cardShadow .block1", 1, {
+         rotation:0,
+         opacity:1,
+         ease: Expo.easeInOut
+      });
+      TweenMax.staggerFrom(".block1 form > *",1, {
+         y:-50,
+         delay:0.5,
+         scale:0.5,
+          opacity: 0,
+          ease: Expo.easeInOut
+       },0.2);
+       TweenMax.from(".wrapper .cardShadow .circle1", 1.5, {
+         scale:0.5,
+         transformOrigin:'top left',
+          opacity: 0,
+          ease: Expo.easeInOut
+       },'-=1');
+      
+       TweenMax.from(".wrapper .cardShadow .circle2", 1.5, {
+         scale:0.5,
+         transformOrigin:'bottom right',
+          opacity: 0,
+          ease: Expo.easeInOut
+       },'-=1');
+}
 
-  // helper
-  // --------------
-
-  function hideCurrent(callback) {
-    inputContainer.style.opacity = 0
-    inputProgress.style.transition = 'none'
-    inputProgress.style.width = 0
-    setTimeout(callback, wTime)
-  }
-
-  function showCurrent(callback) {
-    inputContainer.style.opacity = 1
-    inputProgress.style.transition = ''
-    inputProgress.style.width = '100%'
-    setTimeout(callback, wTime)
-  }
-
-  function transform(x, y) {
-    register.style.transform = 'translate(' + x + 'px ,  ' + y + 'px)'
-  }
-
-  function ok(callback) {
-    register.className = ''
-    setTimeout(transform, tTime * 0, 0, 10)
-    setTimeout(transform, tTime * 1, 0, 0)
-    setTimeout(callback,  tTime * 2)
-  }
-
-  function wrong(callback) {
-    register.className = 'wrong'
-    for(var i = 0; i < 6; i++) // shaking motion
-      setTimeout(transform, tTime * i, (i%2*2-1)*20, 0)
-    setTimeout(transform, tTime * 6, 0, 0)
-    setTimeout(callback,  tTime * 7)
-  }
-
-}())
